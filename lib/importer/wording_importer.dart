bde068dc21a9f07d8ba8a43457c00151374d3bcb
Romain Lagorsse <rlagorsse@betomorrow.com>
2025-07-21 17:21:00 +0200

    Import wordings from ARB files

---
+++ lib/importer/wording_importer.dart
@@ -0,0 +1,6 @@
+import 'package:sync_wording/wording.dart';
+
+abstract class WordingImporter {
+  /// Generic method to call to import the translation files used by the project
+  Future<Wordings> import(Map<String, String> localeFiles);
+}
---
+++ test/importer/arb_round_trip_test.dart
@@ -0,0 +1,101 @@
+import 'dart:io';
+
+import 'package:sync_wording/exporter/arb/arb_wording_exporter.dart';
+import 'package:sync_wording/importer/arb/arb_wording_importer.dart';
+import 'package:sync_wording/wording.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ARB Round Trip', () {
+    late ARBWordingImporter importer;
+    late ARBWordingExporter exporter;
+    late Directory tempDir;
+
+    setUp(() {
+      importer = ARBWordingImporter();
+      exporter = ARBWordingExporter();
+      tempDir = Directory.systemTemp.createTempSync('arb_round_trip_test');
+    });
+
+    tearDown(() {
+      tempDir.deleteSync(recursive: true);
+    });
+
+    test('should maintain data integrity through import/export cycle',
+        () async {
+      final originalWordings = <String, LanguageWordings>{
+        'en': {
+          'welcome': WordingEntry(
+            'Hello {user}',
+            [PlaceholderCharac('user', 'String', null)],
+          ),
+          'today': WordingEntry(
+            'It\'s {date}',
+            [
+              PlaceholderCharac(
+                  'date', 'DateTime', 'dd/MM/yyyy:hh\'h\'mm\'m\'ss')
+            ],
+          ),
+          'simple': WordingEntry('No placeholders', null),
+        },
+        'fr': {
+          'welcome': WordingEntry(
+            'Bonjour {user}',
+            [PlaceholderCharac('user', 'String', null)],
+          ),
+          'today': WordingEntry(
+            'C\'est {date}',
+            [
+              PlaceholderCharac(
+                  'date', 'DateTime', 'dd/MM/yyyy:hh\'h\'mm\'m\'ss')
+            ],
+          ),
+        },
+      };
+
+      final exportedFiles = <String, String>{};
+
+      for (final locale in originalWordings.keys) {
+        final outputFile = '${tempDir.path}/$locale.arb';
+        await exporter.export(locale, originalWordings[locale]!, outputFile);
+        exportedFiles[locale] = outputFile;
+      }
+
+      final importedWordings = await importer.import(exportedFiles);
+
+      expect(importedWordings.length, equals(originalWordings.length));
+
+      for (final locale in originalWordings.keys) {
+        final original = originalWordings[locale]!;
+        final imported = importedWordings[locale]!;
+
+        expect(imported.length, equals(original.length));
+
+        for (final key in original.keys) {
+          final originalEntry = original[key]!;
+          final importedEntry = imported[key]!;
+
+          expect(importedEntry.value, equals(originalEntry.value));
+
+          if (originalEntry.placeholderCharacs != null) {
+            expect(importedEntry.placeholderCharacs, isNotNull);
+            expect(importedEntry.placeholderCharacs!.length,
+                equals(originalEntry.placeholderCharacs!.length));
+
+            for (int i = 0; i < originalEntry.placeholderCharacs!.length; i++) {
+              final originalCharac = originalEntry.placeholderCharacs![i];
+              final importedCharac = importedEntry.placeholderCharacs![i];
+
+              expect(importedCharac.placeholder,
+                  equals(originalCharac.placeholder));
+              expect(importedCharac.type, equals(originalCharac.type));
+              expect(importedCharac.format, equals(originalCharac.format));
+            }
+          } else {
+            expect(importedEntry.placeholderCharacs, isNull);
+          }
+        }
+      }
+    });
+  });
+}
